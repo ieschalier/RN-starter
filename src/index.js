24957405a54db1b9e21381b28e43aefe4c8f5227
igor13127 <igor.eschalier@gmail.com>
2018-01-13 12:18:13 +0100

    Prettier-eslint install

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react'
 
 const test = 'toto'
 
-const Index = () => (
-  <toto id="hello">
-    {`hello ${test}`}
-  </toto>
-)
+const Index = () => <toto id="hello">{`hello ${test}`}</toto>
 
 export default Index
